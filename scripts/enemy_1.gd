0202c801382816bb87423c4b15add3bbd6be6763
Matheus Vicente Rodrigues da Silva <mathewvicente@gmail.com>
2024-05-16 00:01:00 -0300

    Setup prototype first level

---
+++ scripts/enemy_1.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+const SPEED = 40
+
+var direction = 1
+
+var can_check = true
+
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+@onready var ray_cast_down = $RayCastDown
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var timer = $Timer
+
+func _ready():
+	timer.timeout.connect(_on_timer_timeout)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):	
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	
+	if not ray_cast_down.is_colliding() and can_check:
+		direction *= -1
+		animated_sprite.flip_h = not animated_sprite.flip_h
+		can_check = false
+		timer.start()
+		print('Dentro do if, can check: ' + str(can_check))
+		
+	position.x += direction * SPEED * delta
+	
+func _on_timer_timeout():
+	can_check = true
+	print('Dentro da function, can check: ' + str(can_check))
